67fddd51d01c14730e50ce8f720b2b26b7514aec
Gabi3312 <gabioros251@gmail.com>
2024-09-21 15:46:38 -0300

    WDD131

--- Scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-// Para el año actual
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Para la ultima fecha modificada
-document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
